57d10ea1bf0834d11c65c274d7304a386c7154fc
PPC2001 <pratik2612001@gmail.com>
2025-04-19 12:30:21 +0530

    Code added by Pratik

---
+++ src/main/java/com/springboot_with_gcp/SpringbootWithGcpApplication.java
@@ -0,0 +1,13 @@
+package com.springboot_with_gcp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootWithGcpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootWithGcpApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/springboot_with_gcp/SpringbootWithGcpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot_with_gcp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootWithGcpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
